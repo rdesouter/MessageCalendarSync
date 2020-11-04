1bb54fbecdc5acddc32e180ac4b1e268613c9efc
de souter <ronald.desouter.external@atos.net>
2020-11-04 17:04:14 +0100

    add meth for send email with multipart

--- src/main/java/com/rdesouter/message/GmailExploreApplication.java
+++ src/main/java/com/rdesouter/message/GmailExploreApplication.java
@@ -20,10 +20,13 @@ import com.google.api.services.gmail.model.MessagePartHeader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import javax.mail.MessagingException;
-//import javax.mail.Session;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
@@ -32,8 +35,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+import static javax.mail.Message.RecipientType.TO;
+
 @SpringBootApplication
-public class GmailExploreApplication {
+public class GmailExploreApplication implements MessageConstant {
 
     private static final String APPLICATION_NAME = "Noron Gmail API";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
@@ -150,49 +155,64 @@ public class GmailExploreApplication {
         }
     }
 
-//    private static void sendGmail(Gmail gmailService) throws MessagingException, IOException {
-//        MimeMessage mimeMessage = createEmail("<email of destination>", "<email of sender>", "Test lalala", "body of email");
-//        Message response = sendMessage(gmailService, "<email of user>", mimeMessage);
-//    }
 
-//    public static MimeMessage createEmail(String to,
-//                                          String from,
-//                                          String subject,
-//                                          String bodyText)
-//            throws MessagingException {
-//        Properties props = new Properties();
-//        Session session = Session.getDefaultInstance(props, null);
-//
-//        MimeMessage email = new MimeMessage(session);
-//
-//        email.setFrom(new InternetAddress(from));
-//        email.addRecipient(javax.mail.Message.RecipientType.TO,
-//                new InternetAddress(to));
-//        email.setSubject(subject);
-//        email.setText(bodyText);
-//        return email;
-//    }
+    private static void sendGmail(Gmail gmailService) throws MessagingException, IOException {
+//        MimeMessage mimeMessage = createEmail("ron.desouter@gmail.com", "dev@papymousse.be", "First send with api", "Send from dev email");
+        MimeMessage m = createMessageWithMultiPart(SENDER, RECEIVER, SUBJECT);
+        sendMessage(gmailService, "me", m);
+    }
 
-//    private static Message createMessageWithEmail(MimeMessage emailContent)
-//            throws MessagingException, IOException {
-//        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-//        emailContent.writeTo(buffer);
-//        byte[] bytes = buffer.toByteArray();
-//        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
-//        Message message = new Message();
-//        message.setRaw(encodedEmail);
-//        return message;
-//    }
+    public static MimeMessage createMessageWithMultiPart(String to, String from, String sub) throws MessagingException {
+        Session session = Session.getDefaultInstance(new Properties(), null);
 
-//    private static Message sendMessage(Gmail service,
-//                                      String userId,
-//                                      MimeMessage emailContent)
-//            throws MessagingException, IOException {
-//        Message message = createMessageWithEmail(emailContent);
-//        message = service.users().messages().send(userId, message).execute();
-//
-//        System.out.println("Message id: " + message.getId());
-//        System.out.println(message.toPrettyString());
-//        return message;
-//    }
+        MimeMessage message = new MimeMessage(session);
+        Multipart multipart = new MimeMultipart("alternative");
+
+        MimeBodyPart text = new MimeBodyPart();
+        text.setText(BODY_TITLE + "\n" + BODY_TEXT, "utf-8");
+        multipart.addBodyPart(text);
+
+        MimeBodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent("<h1>" + BODY_TITLE + "</h1><p>" + BODY_TEXT + "</p>", "text/html; charset=utf-8");
+        multipart.addBodyPart(htmlPart);
+
+        message.setFrom(new InternetAddress(from));
+        message.addRecipient(TO, new InternetAddress(to));
+        message.setSubject(sub);
+        message.setContent(multipart);
+
+        return message;
+    }
+
+
+    public static MimeMessage createEmail(String to, String from, String subject, String bodyText) throws MessagingException {
+        Session session = Session.getDefaultInstance(new Properties(), null);
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+
+        return email;
+    }
+
+    private static Message sendMessage(Gmail service, String userId, MimeMessage emailContent) throws MessagingException, IOException {
+        Message message = createMessageWithEmail(emailContent);
+        message = service.users().messages().send(userId, message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+        return message;
+    }
+
+    private static Message createMessageWithEmail(MimeMessage emailContent) throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
 }
